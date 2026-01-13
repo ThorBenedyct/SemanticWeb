0c8b9b9ae3acb75c7708e6e6ff0c843d7054b89f
Thorben <Thorbenedyct@gmail.com>
2026-01-13 20:07:20 +0100

    put logic in a class

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
-
-from rdflib import Graph, URIRef, Literal, Namespace
+from rdflib import Graph, URIRef, Literal, Namespace, RDFS
 from rdflib.namespace import RDF, XSD
 import sys
-
 REFERENCE_FILE = "reference-kg.nt"
 HIERARCHY_FILE = "classHierarchy.nt"
 INPUT_FILE = "fokg-sw-test-2024.nt"
@@ -19,59 +17,84 @@ def load_graph(path):
     return g
 
 
-def get_real_score(facts, fact):
-    real_score_literal = facts.value(fact, HAS_TRUTH)
-    if real_score_literal is None:
+class FactChecker:
+    def __init__(self):
+        print("Lade Referenz-KG")
+        self.ref_kg = load_graph(REFERENCE_FILE)
+
+        print("Lade Klassen-Hierachy")
+        self.class_kg = load_graph(HIERARCHY_FILE)
+
+        print("Lade Input-Fakten")
+        self.facts = load_graph(INPUT_FILE)
+
+        self.resultGraph = Graph()
+
+    def get_real_score(self, fact):
+        """For testing purpose only. Uses veracity in test file to check"""
+        real_score_literal = self.facts.value(fact, HAS_TRUTH)
+        if real_score_literal is None:
+            return 0.0
+        return float(real_score_literal[0])
+
+    def get_all_superclasses(self, cls):
+        supers = set()
+        stack = [cls]
+
+        while stack:
+            current = stack.pop()
+            for parent in self.class_kg.objects(current, RDFS.subClassOf):
+                if parent not in supers:
+                    supers.add(parent)
+                    stack.append(parent)
+
+        return supers
+
+    def check_truth(self, fact):
+        sub = self.facts.value(fact, RDF.subject)
+        pre = self.facts.value(fact, RDF.predicate)
+        obj = self.facts.value(fact, RDF.object)
+
+        if sub is None or pre is None or obj is None:
+            return 0.0
+
+        # Existenzprüfung im Referenz-KG
+        is_true = (sub, pre, obj) in self.ref_kg
+
+        if is_true:
+            return 1.0
+
+
         return 0.0
-    return float(real_score_literal[0])
 
-def check_truth(ref_kg, facts, fact):
-    sub = facts.value(fact, RDF.subject)
-    pre = facts.value(fact, RDF.predicate)
-    obj = facts.value(fact, RDF.object)
+    def main(self):
+        test_score = 0.0
+        amount_facts = 0
 
-    if sub is None or pre is None or obj is None:
-        return 0.0
+        for fact in self.facts.subjects(RDF.type, RDF.Statement):
+            score = self.check_truth(fact)
 
-    # Existenzprüfung im Referenz-KG
-    is_true = (sub, pre, obj) in ref_kg
+            real_score = self.get_real_score(fact)
+            test_score = test_score + abs(score - real_score)
+            amount_facts += 1
 
-    if is_true:
-        return 1.0
-    return 0.0
+            self.resultGraph.add((
+                fact,
+                HAS_TRUTH,
+                Literal(score, datatype=XSD.double)
+            ))
+        test_score = test_score / amount_facts
+
+        print("Schreibe Ergebnisdatei")
+        self.resultGraph.serialize(destination=OUTPUT_FILE, format="nt")
+
+        print(f"Abweichung: {test_score}")
+        print("Fertig.")
 
 
 def main():
-    test_score = 0.0
-    amount_facts = 0
-
-    print("Lade Referenz-KG")
-    ref_kg = load_graph(REFERENCE_FILE)
-
-    print("Lade Input-Fakten")
-    facts = load_graph(INPUT_FILE)
-
-    resultGraph = Graph()
-
-    for fact in facts.subjects(RDF.type, RDF.Statement):
-        score = check_truth(ref_kg, facts, fact)
-
-        real_score = get_real_score(facts, fact)
-        test_score = test_score + abs(score-real_score)
-        amount_facts += 1
-
-        resultGraph.add((
-            fact,
-            HAS_TRUTH,
-            Literal(score, datatype=XSD.double)
-        ))
-    test_score = test_score / amount_facts
-
-    print("Schreibe Ergebnisdatei")
-    resultGraph.serialize(destination=OUTPUT_FILE, format="nt")
-
-    print(f"Abweichung: {test_score}")
-    print("Fertig.")
+    fact_checker = FactChecker()
+    fact_checker.main()
 
 
 if __name__ == "__main__":
